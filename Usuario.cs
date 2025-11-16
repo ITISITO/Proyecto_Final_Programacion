1995dd8b6ea29ca09371a03a964f68deaeb2e499
Edisson Benavides <velascoedisonbenavides@gmail.com>
2025-11-16 16:08:09 -0500

    Agrege las clases nada mas

---
+++ Usuario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_de_pedidos_restaurante_PF
+{
+    internal class Usuario
+    {
+    }
+}
